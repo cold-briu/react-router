18fafaccba0e84d7780b52cbbf061409ece1eafe
Andrés Fernández <andresavena19@gmail.com>
2023-01-13 18:00:08 -0500

    feat: added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
 import Title from './Title'
+import Navbar from './Navbar'
 
 import {
-	createBrowserRouter,
-	RouterProvider,
+	Route,
+	Routes,
+	BrowserRouter
 } from "react-router-dom";
 
+export const ROUTES = {
+	HOME: "/",
+	PRODUCTS: "/products",
+	PROFILE: "/profile"
+}
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Title text={"home"} />,
-	},
-]);
 
 const App = () => {
-	return <RouterProvider router={router} />
+	return <>
+		<BrowserRouter>
+
+			<Navbar />
+
+			<Routes>
+				<Route path={ROUTES.HOME} element={<Title text={"Home"} />} />
+				<Route path={ROUTES.PRODUCTS} element={<Title text={"Products"} />} />
+				<Route path={ROUTES.PROFILE} element={<Title text={"Profile"} />} />
+				<Route path="*" element={<Title text={"not found"} />} />
+			</Routes>
+		</BrowserRouter>
+
+	</>
 
 }
 
